6d6128df55fb218946d438971970c70146abcf5c
RevingtonDev <revington.dev@gmail.com>
2022-07-07 14:58:53 +0530

    Student page.

---
+++ src/pages/Student.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class Student extends Component {
+
+    render() {
+        return (
+            <>
+                <h1>Hello, World!</h1>
+            </>
+        )
+    }
+}
